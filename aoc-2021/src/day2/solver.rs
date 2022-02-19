ba04e4b153a03f366e94862b42ea30149ddeda76
Dione <dionemorales@outlook.com>
2022-02-20 09:01:19 +1100

    solution for day 2 part 2

--- aoc-2021/src/day2/solver.rs
+++ aoc-2021/src/day2/solver.rs
@@ -37,7 +37,6 @@ impl Day2Solver {
         let base_path = std::env::current_dir().expect("Current directory not found");
         let file_path = base_path.join(&input_file);
         let input = fs::read_to_string(file_path).expect("Failed to read file");
-        let instructions = parse_instructions(&input);
         Day2Solver {
             input,
         }
@@ -55,12 +54,37 @@ impl Day2Solver {
         });
         (horizontal_position,depth)
     }
+
+    fn calculate_course_part_2(instructions: InstructionList) -> (i64, i64) {
+        let mut horizontal_position = 0;
+        let mut depth = 0;
+        let mut aim = 0;
+        instructions.for_each(|instruction| {
+            match instruction {
+                Instruction::Down(unit) => aim = aim + unit,
+                Instruction::Up(unit) => aim = aim - unit,
+                Instruction::Forward(unit) => {
+                    horizontal_position = horizontal_position + unit;
+                    depth = depth + aim * unit;
+                },
+            }
+        });
+        (horizontal_position,depth)
+    }
+
 }
 
 impl ChallengeSolver for Day2Solver {
     fn solve_part_one(&self) -> String {
         let instructions = parse_instructions(&self.input);
         let (horizontal, depth) = Day2Solver::calculate_course(instructions);
+        let area = horizontal * depth;
+        area.to_string()
+    }
+
+    fn solve_part_two(&self) -> String {
+        let instructions = parse_instructions(&self.input);
+        let (horizontal, depth) = Day2Solver::calculate_course_part_2(instructions);
         let area = horizontal * depth;
         area.to_string()
     }
@@ -83,5 +107,19 @@ forward 2"#.to_string();
             input
         };
         assert_eq!(solver.solve_part_one().parse::<i64>().unwrap(), 150)
+    }
+
+    #[test]
+    fn part_2_example_1() {
+        let input = r#"forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"#.to_string();
+        let solver = Day2Solver {
+            input
+        };
+        assert_eq!(solver.solve_part_two().parse::<i64>().unwrap(), 900)
     }
 }
